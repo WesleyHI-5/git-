0f6f771d2d6c52e0632b6a3898fa8b1516c5c4c2
Wesley Natnael da Silva Lacerda <wesleylacerda99@gmail.com>
2020-10-03 02:51:48 -0300

    Criado o meu preimeiro projeto-web

---
+++ site-inicial/scripts/main.js
@@ -0,0 +1,17 @@
+var carro = { carros: {a: "Saab", "b": "Jeep"}, 7: "Mazda" };
+
+console.log(carro.carros.b); // Jeep
+console.log(carro[7]); // Mazda
+
+
+var automovel = {carros: {a: "Fiat", b: "Pegeuot"}, 8: "Celta"};
+console.log(automovel.carros.b);
+console.log(automovel[8]);
+"pão \251";
+
+var poema = 
+"Rosas são vermelhas\n\
+Violetas são azuis,\n\
+Esse seu sorriso\n\
+é o que me seduz. (Wesley Lacerdinas)"
+console.log(poema);
