901c9e1e9c62d3caadf4862aaf27270f41ddf663
Wesley <wesleylacerda99@gmail.com>
2020-10-07 13:54:56 -0300

    Adicionei funcionalidades ao site

--- site-inicial/scripts/main.js
+++ site-inicial/scripts/main.js
@@ -1,17 +1,25 @@
-var carro = { carros: {a: "Saab", "b": "Jeep"}, 7: "Mazda" };
+let minhaImagem = document.querySelector('img');
 
-console.log(carro.carros.b); // Jeep
-console.log(carro[7]); // Mazda
-
-
-var automovel = {carros: {a: "Fiat", b: "Pegeuot"}, 8: "Celta"};
-console.log(automovel.carros.b);
-console.log(automovel[8]);
-"pão \251";
-
-var poema = 
-"Rosas são vermelhas\n\
-Violetas são azuis,\n\
-Esse seu sorriso\n\
-é o que me seduz. (Wesley Lacerdinas)"
-console.log(poema);
+minhaImagem.onclick = function() {
+    let meuSrc = minhaImagem.getAttribute('src');
+    if(meuSrc === 'imagens/d.png') {
+      minhaImagem.setAttribute ('src','imagens/cachorro.png');
+    } else {
+      minhaImagem.setAttribute ('src','imagens/d.png');
+    }
+}
+let meuBotao = document.querySelector('button');
+let meuCabecalho = document.querySelector('h1');
+function defineNomeUsuario() {
+    let meuNome = prompt('Por favor, digite seu nome.');
+    localStorage.setItem('nome', meuNome);
+    meuCabecalho.textContent = 'Animais são legais, ' + meuNome;
+  }
+  if(!localStorage.getItem('nome')) {
+    defineNomeUsuario();
+  } else {
+    let nomeGuardado = localStorage.getItem('nome');
+    meuCabecalho.textContent = 'Animais são legais, ' + nomeGuardado;
+  }
+  meuBotao.onclick = function() { defineNomeUsuario();
+  }
